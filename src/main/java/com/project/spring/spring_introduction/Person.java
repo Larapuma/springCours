2a606d16bf15c5c20369e2e08394ffbf65247825
kaneki4kaken <N.plechkov@yandex.ru>
2024-12-26 15:18:18 +0300

    DI с сеттером

--- src/main/java/com/project/spring/spring_introduction/Person.java
+++ src/main/java/com/project/spring/spring_introduction/Person.java
@@ -3,12 +3,20 @@ package com.project.spring.spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
-
-        System.out.println("Бин человека появился!");
-
+//    public Person(Pet pet) {
+//
+//        System.out.println("Class Person: set pet");
+//
+//        this.pet = pet;
+//    }
+    Person(){
+        System.out.println("Class Person: set pet");
+    }
+    //pet -> setPet
+    public void setPet(Pet pet) {
         this.pet = pet;
     }
+
     public void callYourPet(){
         System.out.println("Hello, my Pet!");
         pet.say();
