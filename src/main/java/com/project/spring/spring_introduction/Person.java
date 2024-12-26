fb8d2992fd59bfdfa49508f424c51af0d958ae3c
kaneki4kaken <N.plechkov@yandex.ru>
2024-12-26 15:30:17 +0300

    DI внедрение строк и других значений (вручную).

--- src/main/java/com/project/spring/spring_introduction/Person.java
+++ src/main/java/com/project/spring/spring_introduction/Person.java
@@ -1,20 +1,37 @@
 package com.project.spring.spring_introduction;
 
+import lombok.Getter;
+
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
-//    public Person(Pet pet) {
-//
-//        System.out.println("Class Person: set pet");
-//
-//        this.pet = pet;
-//    }
+
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname.");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age.");
+        this.age = age;
+    }
+
     Person(){
         System.out.println("Class Person: set pet");
     }
     //pet -> setPet
     public void setPet(Pet pet) {
         this.pet = pet;
+    }
+    public String getSurname() {
+        return surname;
     }
 
     public void callYourPet(){
--- src/main/java/com/project/spring/spring_introduction/Test3.java
+++ src/main/java/com/project/spring/spring_introduction/Test3.java
@@ -9,6 +9,7 @@ public class Test3 {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         Person person = context.getBean("myPerson",Person.class);
         person.callYourPet();
+        System.out.println(person.getAge() +" " + person.getSurname());
         context.close();
     }
 }
