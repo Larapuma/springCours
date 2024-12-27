501b5575a657b2332e9c86a05c2a67e79fe0ab67
kaneki4kaken <N.plechkov@yandex.ru>
2024-12-27 21:19:54 +0300

    Qualifier. Или что делать, если есть много аннотаций Autowired

--- src/main/java/com/project/spring/spring_introduction/Person.java
+++ src/main/java/com/project/spring/spring_introduction/Person.java
@@ -2,19 +2,21 @@ package com.project.spring.spring_introduction;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBin")
 public class Person {
+
     private Pet pet;
     private String surname;
     private int age;
 
-//    @Autowired
-//    public Person(Pet pet){
-//        System.out.println("Person bin is created");
-//        this.pet = pet;
-//    }
+    @Autowired
+    public Person(@Qualifier("dog") Pet pet){
+        System.out.println("Person bin is created");
+        this.pet = pet;
+    }
     public void setSurname(String surname) {
         System.out.println("Class Person: set surname.");
         this.surname = surname;
@@ -34,11 +36,13 @@ public class Person {
     }
 
     //pet -> setPet
-    @Autowired
-    public void setPet(Pet pet) {
-        this.pet = pet;
-        System.out.println("Class Person: set pet");
-    }
+
+//    @Autowired
+//    @Qualifier("catBean")
+//    public void setPet(Pet pet) {
+//        this.pet = pet;
+//        System.out.println("Class Person: set pet");
+//    }
     public String getSurname() {
         return surname;
     }
